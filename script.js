113e40b039f3f3a07e7f87d0050fd39c912685b1
Rafael <inversionesdigitales@hotmail.es>
2023-07-14 10:54:07 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var redBox = document.getElementById("red");
+var greenBox = document.getElementById("green");
+var blueBox = document.getElementById("blue");
+
+redBox.style.backgroundColor = "red";
+greenBox.style.backgroundColor = "green";
+blueBox.style.backgroundColor = "blue";
